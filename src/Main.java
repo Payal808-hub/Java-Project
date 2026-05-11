4749e72840c2e41d358cf7785d8821d5a8c4162e
Payal808-hub <damarepayal694@gmail.com>
2026-05-11 15:27:05 +0530

    added SOP

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and main");
         System.out.printf("Hello From branch1");
+        System.out.printf("Hello From branch1");
 
 
     }
